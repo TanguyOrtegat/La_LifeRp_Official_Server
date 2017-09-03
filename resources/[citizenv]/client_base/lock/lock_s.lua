17ac3878bacd59c9f16a9aa58ed61d3136588de5
Shakir Darwish <michaelp.parent@hotmail.com>
2017-09-03 23:40:14 +0100

    Ajout propri LS 3/5 + lock jos

--- resources/[citizenv]/client_base/lock/lock_s.lua
+++ resources/[citizenv]/client_base/lock/lock_s.lua
@@ -5,7 +5,8 @@ AddEventHandler('lock:getCar', function(callback)
     TriggerEvent('es:getPlayerFromId', playerSource, function(user)
         if (user) then
             local plateveh = string.upper("CTZN" .. string.sub(user.identifier, -4))
-            TriggerClientEvent("lock:f_getCar", playerSource, {plateveh, user.jobVehicle})
+            local plateJob = string.upper("JOBS" .. string.sub(user.identifier, -4))
+            TriggerClientEvent("lock:f_getCar", playerSource, {plateveh, plateJob})
         else
             TriggerEvent("es:desyncMsg", "lock_s.lua GetCar")
         end
